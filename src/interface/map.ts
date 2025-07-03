d5e1b17854162bc9bda247a8af720a9eedcc5ede
bankjirapan <jirapan_yankhan@hotmail.com>
2025-07-03 16:38:35 +0700

    chore: add map type

---
+++ src/interface/map.ts
@@ -0,0 +1,12 @@
+export interface Map {
+    zoom: (zoomLevel: number) => void;
+    location: (location: { lon: number; lat: number }, animate?: boolean) => void;
+    Overlays: {
+        add: (overlay: any) => void;
+        remove: (overlay: any) => void;
+    };
+    Event: {
+        bind: (eventName: string, callback: () => void) => void;
+    };
+    lastview?: boolean;
+}
